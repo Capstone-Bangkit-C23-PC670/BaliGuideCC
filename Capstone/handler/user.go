fdb9d6c81b56d44f096ad3e03260d0da32577ddc
azharsalsabilaa <128112570+azharsalsabilaa@users.noreply.github.com>
2023-06-16 21:36:20 +0700

    Add files via upload

---
+++ Capstone/handler/user.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"mygram/helper"
+	"mygram/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIresponse(http.StatusUnprocessableEntity, errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIresponse(http.StatusUnprocessableEntity, nil)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+	formatter := user.FormatterUser(newUser, "")
+	response := helper.APIresponse(http.StatusOK, formatter)
+	c.JSON(http.StatusOK, response)
+}
---
+++ Capstone/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	food "mygram/Food"
+	"mygram/cafe"
+	"mygram/handler"
+	"mygram/hotel"
+	"mygram/souvenir"
+	"mygram/touris"
+	"mygram/user"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	dsn := "root:password@tcp(34.101.160.135:3306)/capstone-new?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db Connestion Error")
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+	hotelRepository := hotel.NewRepository(db)
+	hotelService := hotel.NewService(hotelRepository)
+	hotelHandler := handler.NewHotelHandler(hotelService)
+	tourisRepository := touris.NewRepository(db)
+	tourisService := touris.NewService(tourisRepository)
+	tourisHandler := handler.NewTourisHandler(tourisService)
+	cafeRepository := cafe.NewRepository(db)
+	cafeService := cafe.NewService(cafeRepository)
+	cafeHandler := handler.NewCafeHandler(cafeService)
+	souvenirRepository := souvenir.NewRepository(db)
+	souvenirService := souvenir.NewService(souvenirRepository)
+	souvenirHandler := handler.NewSouvenirHandler(souvenirService)
+	foodsRepository := food.NewRepository(db)
+	foodsService := food.NewService(foodsRepository)
+	foodsHandler := handler.NewFoodHandler(foodsService)
+
+	router := gin.Default()
+	api := router.Group("/users/v1")
+	apiHotel := router.Group("/hotels")
+	apiTouris := router.Group("/touris")
+	apiCafe := router.Group("/cafe")
+	apiSouvenir := router.Group("/souvenir")
+	apiFoods := router.Group("food")
+
+	api.POST("/user", userHandler.RegisterUser)
+	apiHotel.GET("/", hotelHandler.GetHotel)
+	apiHotel.GET("/rating", hotelHandler.GetRate)
+	apiTouris.GET("/", tourisHandler.GetTouris)
+	apiTouris.GET("/rate", tourisHandler.GetRate)
+	apiCafe.GET("/", cafeHandler.GetCafe)
+	apiCafe.GET("/rate", cafeHandler.GetRate)
+	apiSouvenir.GET("/", souvenirHandler.GetSouvenir)
+	apiSouvenir.GET("/rate", souvenirHandler.GetRate)
+	apiFoods.GET("/", foodsHandler.GetFoods)
+	apiFoods.GET("/rate", foodsHandler.GetRate)
+
+	router.Run(":8080")
+}
